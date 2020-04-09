fe90ff79762f4e0b7b87f5d8577ba76d27678efb
User1 <user@example.com>
2020-04-09 10:40:05 -0500

    updates

--- map1/map1.js
+++ map1/map1.js
@@ -1,1 +1,10 @@
 var NOLA = L.map(NOLA).setView([62.18, -98.14], 8)
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(NOLA)
+var point = L.marker([60, -90]).addTo(NOLA)
+var circle = L.polygon([
+  [50, -100],
+  [50, -100],
+  [50, -99]
+]).addTo(NOLA)
+circle.bindPopup('area1.')
+point.bindPopup('boo.')
