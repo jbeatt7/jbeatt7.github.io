14b7d3ffa46ef3dc89315eabc32c0c8f83dc0af8
User1 <user@example.com>
2020-04-16 07:34:13 -0500

    ..

--- map2/map2.js
+++ map2/map2.js
@@ -6,3 +6,9 @@ L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(map2)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(map2)
