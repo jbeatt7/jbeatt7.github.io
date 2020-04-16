68c2151eb59a2abebbf319a208c49ee83b582482
User1 <user@example.com>
2020-04-15 20:58:07 -0500

    updooooots

--- map1/map1.js
+++ map1/map1.js
@@ -1,10 +1,10 @@
 var map1 = L.map("map1").setView([62.18, -100.14], 4)
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo("map1")
-var point1 = L.marker([60, -100]).addTo("map1")
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map1)
+var Marker = L.marker([60, -100]).addTo(map1)
 var Circle = L.polygon([
   [61, -88],
   [65, -88],
   [52, -82]
-]).addTo("map1")
+]).addTo(map1)
 Circle.bindPopup('Hi')
-point1.bindPopup('Boo')
+Marker.bindPopup('Boo')
